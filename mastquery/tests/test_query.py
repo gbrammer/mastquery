9813332df5524374ed001f6e5033b289a25f07f9
Gabriel Brammer <gbrammer@gmail.com>
2022-05-17 21:12:10 +0200

    test overlaps

--- mastquery/tests/test_query.py
+++ mastquery/tests/test_query.py
@@ -1,13 +1,29 @@
+import os
+import glob
+import matplotlib.pyplot as plt
 
-from .. import query
+from .. import query, overlaps
 
 def test_query():
     """
     Test basic query
     """
     _res = query.run_query(proposal_id=[11359], filters=['G141'])
-
     assert(len(_res) == 1)
+    
+    olap = overlaps.find_overlaps(_res, proposal_id=[11359], 
+                                  instruments=['WFC3/IR'])
+    assert(len(olap) == 1)
+    
+    _ = overlaps.split_associations(olap[0])
+    
+    plt.close('all')
+    
+    # cleanup
+    files = glob.glob(f"{olap[0].meta['NAME']}*")
+    for file in files:
+        os.remove(file)
+        
     return _res
 
 def test_products():
