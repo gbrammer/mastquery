8d8d6e52abf997fdf02366bdc06989b014e2501b
Gabriel Brammer <gbrammer@gmail.com>
2022-08-11 23:48:58 +0200

    back to git version

--- mastquery/version.py
+++ mastquery/version.py
@@ -1,4 +1,4 @@
 # git describe --tags
 __version__ = "1.4"
-__long_version__ = "1.3.0-2-g48c349c"
-__version_hash__ = "g48c349c"
+__long_version__ = "1.4"
+__version_hash__ = "---"
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 #version = "1.1" # mast products workaround
 # version = "1.2" # jwst
 # version = "1.3" # polystr
-version = "1.4" # force_rate in fetch
+# version = "1.4" # force_rate in fetch
 
 # Set this to true to add install_requires to setup
 # Turned off for incremental builds as it kills "reload(mastquery.query)" 
